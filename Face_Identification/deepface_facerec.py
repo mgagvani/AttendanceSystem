ae61103aebec69c732d6a655f4d837e666fdee52
Manav Gagvani <mgagvani@outlook.com>
2023-12-01 11:44:08 -0500

    change minimum size and add some nice things

--- Face_Identification/deepface_facerec.py
+++ Face_Identification/deepface_facerec.py
@@ -1,5 +1,7 @@
 from deepface import DeepFace
 
+from stream_face_identification import *
+
 MODELS = [
   "VGG-Face", 
   "Facenet", 
@@ -15,14 +17,18 @@ MODELS = [
 def main():
     # See https://github.com/serengil/deepface/blob/master/deepface/commons/realtime.py
     # for implementation of stream()
-    DeepFace.stream(
+
+    analysis(
         db_path = 'test2',
         model_name = MODELS[6], # ArcFace
         detector_backend = 'opencv',
         enable_face_analysis = False,
         time_threshold = 3,
         frame_threshold = 3,
-        distance_metric = 'cosine'
+        source=0,
+        distance_metric = 'cosine',
+        face_size_threshold = 100,
+        enable_viz=False,
     )
 
 if __name__ == "__main__":
