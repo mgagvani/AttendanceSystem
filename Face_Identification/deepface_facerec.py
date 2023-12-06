1f09b04be30601bfcaf02e76d3af1eae7592d3f7
Manav Gagvani <mgagvani@outlook.com>
2023-12-06 11:26:09 -0500

    headless version without opencv visualization

--- Face_Identification/deepface_facerec.py
+++ Face_Identification/deepface_facerec.py
@@ -1,6 +1,7 @@
 from deepface import DeepFace
 
 from stream_face_identification import *
+from headless_id import *
 
 MODELS = [
   "VGG-Face", 
@@ -18,20 +19,38 @@ def main():
     # See https://github.com/serengil/deepface/blob/master/deepface/commons/realtime.py
     # for implementation of stream()
 
-    rtmp = "rtmp://127.0.0.1/live/SyWPfOiBa"
+    # rtmp = "rtmp://127.0.0.1/live/SyWPfOiBa"
+    rtmp = "rtmp://172.20.10.3/live/SyWPfOiBa"
     
+    '''
     analysis(
-        db_path = '/Users/pranavv/Library/CloudStorage/GoogleDrive-1823210@fcpsschools.net/My Drive/Machine Learning/Facial Recognition/AttendanceSystem/Face_Identification/test2',
+        # db_path = '/Users/pranavv/Library/CloudStorage/GoogleDrive-1823210@fcpsschools.net/My Drive/Machine Learning/Facial Recognition/AttendanceSystem/Face_Identification/test2',
+        db_path="test2",
         model_name = MODELS[6], # ArcFace
         detector_backend = 'opencv',
         enable_face_analysis = False,
-        time_threshold = 0.1,
-        frame_threshold = 0.1,
+        time_threshold = 1,
+        frame_threshold = 1,
         source=rtmp,
         distance_metric = 'cosine',
         face_size_threshold = 100,
         enable_viz=True,
     )
+    '''
+    face = streamed_id_no_preview(
+            db_path="test2",
+            model_name = MODELS[6], # ArcFace
+            detector_backend = 'opencv',
+            source=rtmp,
+            distance_metric = 'cosine',
+            face_size_threshold = 100,
+        )
+    
+    for i in range(10):
+        t0 = time.perf_counter()
+        recognitions = next(face)
+        t1 = time.perf_counter()
+        print(f"Time taken: {t1-t0:.2f}s", recognitions)
 
 if __name__ == "__main__":
     main()
---
+++ Face_Identification/headless_id.py
@@ -0,0 +1,123 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+import cv2
+from deepface import DeepFace
+from deepface.commons import functions
+
+# dependency configuration
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+# pylint: disable=too-many-nested-blocks
+
+def streamed_id_no_preview(
+    db_path,
+    model_name="ArcFace",
+    detector_backend="opencv",
+    distance_metric="cosine",
+    source=0,
+    face_size_threshold=100,
+):    
+    pivot_img_size = 224  # face recognition result image
+
+    # find custom values for this input set
+    target_size = functions.find_target_size(model_name=model_name)
+    
+    # build models once to store them in the memory
+    # otherwise, they will be built after cam started and this will cause delays
+    DeepFace.build_model(model_name=model_name)
+    print(f"facial recognition model {model_name} is just built")
+
+    # call a dummy find function for db_path once to create embeddings in the initialization
+    DeepFace.find(
+        img_path=np.zeros([pivot_img_size, pivot_img_size, 3]),
+        db_path=db_path,
+        model_name=model_name,
+        detector_backend=detector_backend,
+        distance_metric=distance_metric,
+        enforce_detection=False,
+    )
+
+    cap = cv2.VideoCapture(source)  # webcam
+
+    while True:
+        _, img = cap.read()
+
+        # T0 - Camera read
+        T0 = time.perf_counter()
+
+        if img is None:
+            break
+
+        raw_img = img.copy()
+        resolution_x = img.shape[1]
+        resolution_y = img.shape[0]
+
+
+        # detect faces
+        face_included_frames = 0
+        try:
+            # just extract the regions to highlight in webcam
+            face_objs = DeepFace.extract_faces(
+                img_path=img,
+                target_size=target_size,
+                detector_backend=detector_backend,
+                enforce_detection=False,
+            )
+            faces = []
+            for face_obj in face_objs:
+                facial_area = face_obj["facial_area"]
+                faces.append(
+                    (
+                        facial_area["x"],
+                        facial_area["y"],
+                        facial_area["w"],
+                        facial_area["h"],
+                    )
+                )
+        except:  # to avoid exception if no face detected
+            faces = []
+
+            if len(faces) == 0:
+                face_included_frames = 0
+
+        # T1 - Face detection
+        T1 = time.perf_counter()
+        # print(f"Face detection took {T1 - T0} seconds")
+
+        # for each face, if bigger than threshold, predict
+        for i, face in enumerate(faces):
+            x, y, w, h = face
+
+            if w > face_size_threshold:
+                face_included_frames += 1
+
+                # predict
+                custom_face = raw_img[y : y + h, x : x + w]  # slice face
+
+                dfs = DeepFace.find(
+                    img_path=custom_face,
+                    db_path=db_path,
+                    model_name=model_name,
+                    detector_backend=detector_backend,
+                    distance_metric=distance_metric,
+                    enforce_detection=False,
+                    silent=True,
+                )
+
+                T2 = time.perf_counter()
+                # print(f"Face recognition {i} took {T2 - T1} seconds")
+                T1 = T2
+                
+                if len(dfs) > 0:
+                    # directly access 1st item because custom face is extracted already
+                    df = dfs[0]
+
+                    if df.shape[0] > 0:
+                        candidate = df.iloc[0]
+                        label = candidate["identity"]
+                        yield label
+
+                
+    cap.release()
